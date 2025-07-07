561a0adcaa6f1f74908f7b46fee7ca982276827e
P3T3R2002 <banyai.p0227@gmail.com>
2025-07-08 00:03:50 +0200

    restart

--- project_shooter/src/main.py
+++ project_shooter/src/main.py
@@ -17,49 +17,50 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    updatable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
-    asteroids = pygame.sprite.Group()
-    shoots = pygame.sprite.Group()
-
-    Shoot.containers = (shoots, updatable, drawable)
-
-    Asteroid.containers = (asteroids, updatable, drawable)
-
-    AsteroidField.containers = (updatable)
-    asteroidfield = AsteroidField()
-
-    Player.containers = (updatable, drawable)
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-
-    camera = Camera(player)
-
     while True:
-        screen.fill('black')
-        for thing in updatable:
-            thing.update(dt, camera)
+        updatable = pygame.sprite.Group()
+        drawable = pygame.sprite.Group()
+        asteroids = pygame.sprite.Group()
+        shoots = pygame.sprite.Group()
 
-        for thing in drawable:
-            thing.draw(screen)
+        Shoot.containers = (shoots, updatable, drawable)
 
-        for asteroid in asteroids:
-            if not asteroid.collsion(player):
-                print(f"Game Over!\nYour score: {player.score}")
-                return
+        Asteroid.containers = (asteroids, updatable, drawable)
 
-        for asteroid in asteroids:
-            for bullet in shoots:
-                if not asteroid.collsion(bullet): 
-                    player.score += 1                   
-                    asteroid.split()
-                    bullet.kill()
+        AsteroidField.containers = (updatable)
+        asteroidfield = AsteroidField()
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
+        Player.containers = (updatable, drawable)
+        player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
-        pygame.display.flip()
-        dt = game_time.tick(60)/1000
+        camera = Camera(player)
+        dead = False
+        while not dead:
+            screen.fill('black')
+            for thing in updatable:
+                thing.update(dt, camera)
+
+            for thing in drawable:
+                thing.draw(screen)
+
+            for asteroid in asteroids:
+                if not asteroid.collsion(player):
+                    print(f"Game Over!\nYour score: {player.score}")
+                    dead = True
+
+            for asteroid in asteroids:
+                for bullet in shoots:
+                    if not asteroid.collsion(bullet): 
+                        player.score += 1                   
+                        asteroid.split()
+                        bullet.kill()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+
+            pygame.display.flip()
+            dt = game_time.tick(60)/1000
 
 
 
